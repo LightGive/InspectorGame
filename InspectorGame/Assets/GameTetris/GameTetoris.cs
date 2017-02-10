cc037d208c32ac2834c4abae70583e328979527c
12LJJY1\User <roi.akase@outlook.jp>
2017-02-11 00:37:42 +0900

    テトリス作成開始

---
+++ InspectorGame/Assets/GameTetris/GameTetoris.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameTetoris : MonoBehaviour{ }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(GameTetoris))]
+public class GameTetorisEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+
+	}
+}
+#endif
+
