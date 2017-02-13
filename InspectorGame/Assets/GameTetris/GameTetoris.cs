aa570519ccf07ea03ac17e65c6f66d5a69d51742
12LJJY1\User <roi.akase@outlook.jp>
2017-02-14 00:00:22 +0900

    DeltaTimeのテスト
    GUI.Boxの幅を変えるテスト

--- InspectorGame/Assets/GameTetris/GameTetoris.cs
+++ InspectorGame/Assets/GameTetris/GameTetoris.cs
@@ -12,9 +12,25 @@ public class GameTetoris : MonoBehaviour{ }
 [CustomEditor(typeof(GameTetoris))]
 public class GameTetorisEditor : Editor
 {
+	private float deltaTime;
+	private float prevTime;
+	private float pos;
+
 	public override void OnInspectorGUI()
 	{
+		CalcDeltaTime();
+		GUILayout.Box("a", GUILayout.Width(pos));
+		pos += deltaTime;
+		if (pos > 400)
+			pos = 0;
+		Repaint();
+	}
 
+	void CalcDeltaTime()
+	{
+		float now = (float)EditorApplication.timeSinceStartup;
+		deltaTime = (now - prevTime) * 800;
+		prevTime = now;
 	}
 }
 #endif
