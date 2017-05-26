8cafc540475d319557519876161777e092a5b8a9
12LJJY1\User <roi.akase@outlook.jp>
2017-05-27 00:45:11 +0900

    ライン消し処理の追加

--- InspectorGame/Assets/GameTetris/GameTetoris.cs
+++ InspectorGame/Assets/GameTetris/GameTetoris.cs
@@ -15,37 +15,43 @@ public class GameTetorisEditor : Editor
 	private const int WALL_NUM = 2;
 	private const int WID_NUM = 12;
 	private const int HEI_NUM = 23;
-	private const int BLOCK_TYPE = 2;
+	private const int BLOCK_TYPE = 3;
 	private const int BLOCK_SPACE = 4;
 	private const int BLOCK_INIT_POS_X = 4;
 	private const int BLOCK_INIT_POS_Y = 0;
-	private const float INTERVAL = 100.5f;
+	private const float INTERVAL = 800.0f;
 
 	private int[,] mass = new int[HEI_NUM, WID_NUM];
 	private int[,] myBlock = new int[BLOCK_SPACE, BLOCK_SPACE];
 	private int myblockPosX = 0;
 	private int myblockPosY = 0;
 	private int score;
-	private bool isStart = false;
-	private bool isGameOver = false;
 	private float deltaTime;
 	private float prevTime;
 	private float pos;
 	private float timeCnt;
+	private bool isStart = false;
+	private bool isGameOver = false;
 
 	private int[,,] massList = new int[BLOCK_TYPE, BLOCK_SPACE, BLOCK_SPACE]
 	{
 		{
-			{0,5,0,0 },
-			{0,5,0,0 },
+			{0,5,5,0 },
+			{0,5,5,0 },
 			{0,5,5,0 },
 			{0,5,5,0 }
 		},
 		{
-			{0,0,0,4 },
-			{0,0,0,4 },
-			{0,0,0,4 },
-			{4,4,4,4 }
+			{3,3,3,3 },
+			{3,3,3,3 },
+			{3,3,3,3 },
+			{3,3,3,3 }
+		},
+		{
+			{3,3,3,3 },
+			{3,3,3,3 },
+			{3,3,3,3 },
+			{3,3,3,3 }
 		},
 	};
 
@@ -54,16 +60,20 @@ public class GameTetorisEditor : Editor
 		Wall = -1,
 		None = 0,
 		Square = 1,
+		Delete = 99,
 	}
 
 	public override void OnInspectorGUI()
 	{
 		if(!isStart)
 		{
-			if (GUILayout.Button("Start"))
-			{
-				Init();
-			}
+			DrawStart();
+			return;
+		}
+
+		if (isGameOver)
+		{
+			DrawGameover();
 			return;
 		}
 
@@ -77,7 +87,9 @@ public class GameTetorisEditor : Editor
 	void Init()
 	{
 		isStart = true;
-		
+		isGameOver = false;
+		score = 0;
+
 		mass = new int[,]
 		{
 			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -104,15 +116,16 @@ public class GameTetorisEditor : Editor
 			{-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1 },
 			{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1 },														
 		};
+
 		myblockPosX = 4;
 		myblockPosY = 0;
+		InstantiateBlock();
 	}
 
 	void DisplayMass()
 	{
 		var windowWidth = EditorGUIUtility.currentViewWidth;
 		var w = (windowWidth / WID_NUM) - 8;
-
 		for (int i = 0; i < HEI_NUM; i++)
 		{
 			EditorGUILayout.BeginHorizontal();
@@ -125,7 +138,12 @@ public class GameTetorisEditor : Editor
 					var y = i - myblockPosY;
 					block = myBlock[y, x];
 				}
-				GUI.color = (block != (int)BlockInfo.None) ? GetColor(block) : GetColor(mass[i, j]);
+
+				if (block != (int)BlockInfo.None && !isGameOver)
+					GUI.color = GetColor(block);
+				else
+					GUI.color = GetColor(mass[i, j]);
+
 				GUILayout.Box("", GUILayout.Width(w), GUILayout.Height(w));
 			}
 			EditorGUILayout.EndHorizontal();
@@ -136,18 +154,25 @@ public class GameTetorisEditor : Editor
 
 	void InstantiateBlock()
 	{
-		myblockPosX = 3;
+		myblockPosX = 4;
 		myblockPosY = 0;
 
 		int blockNo = Random.Range(0, BLOCK_TYPE);
 		myBlock = new int[BLOCK_SPACE, BLOCK_SPACE];
 
-		for(int i = 0; i < BLOCK_SPACE; i++)
+		for (int i = 0; i < BLOCK_SPACE; i++)
 		{
 			for (int j = 0; j < BLOCK_SPACE; j++)
 			{
 				myBlock[i, j] = massList[blockNo, i, j];
 			}
+		}
+
+		isGameOver = !Judge(0, 0);
+		if (isGameOver)
+		{
+			Fixation();
+			GameOver();
 		}
 	}
 
@@ -192,37 +217,67 @@ public class GameTetorisEditor : Editor
 					continue;
 				if (myBlock[j, i] == (int)BlockInfo.None)
 					continue;
-
 				var nextPosX = myblockPosX + i + _dirX;
 				var nextPosY = myblockPosY + j + _dirY;
-
 				if (nextPosX >= WID_NUM || nextPosX < 0 || nextPosY >= HEI_NUM || nextPosY < 0)
 				{
 					isCanMove = false;
 					continue;
 				}
-
 				if (mass[nextPosY, nextPosX] == (int)BlockInfo.Wall||
 					mass[nextPosY, nextPosX] == (int)BlockInfo.Square)
 					isCanMove = false;
 			}
 		}
-
 		return isCanMove;
 	}
+	
 
 	void Fall()
 	{
 		if (!Judge(0, 1))
+		{
 			Fixation();
+			Delete();
+			InstantiateBlock();
+		}
 		else
+		{
 			Move(0, 1);
+		}
 	}
 
 	void Move(int _dirX = 0, int _dirY = 0)
 	{
 		myblockPosX += _dirX;
 		myblockPosY += _dirY;
+	}
+
+	void Delete()
+	{
+		for (int i = HEI_NUM - 1; i > 0; i--)
+		{
+			var cnt = 0;
+			for (int j = 0; j < WID_NUM; j++)
+			{
+				if (mass[i, j] == (int)BlockInfo.Square)
+					cnt++;
+			}
+			if (cnt < 10)
+				continue;
+			for (int ii = i; ii > 0; ii--)
+			{
+				for (int j = 0; j < WID_NUM; j++)
+				{
+					if (mass[ii, j] == (int)BlockInfo.Wall || mass[ii-1, j] == (int)BlockInfo.Wall)
+						continue;
+					mass[ii, j] = mass[ii -1, j];
+				}
+			}
+			for (int j = 0; j < WID_NUM; j++)
+				mass[0, j] = (int)BlockInfo.None;
+			i++;
+		}
 	}
 
 	void Fixation()
@@ -235,18 +290,33 @@ public class GameTetorisEditor : Editor
 					mass[myblockPosY + j, myblockPosX + i] = (int)BlockInfo.Square;
 			}
 		}
+	}
 
-		InstantiateBlock();
+	void GameOver()
+	{
+		EditorUtility.DisplayDialog("Gameover", "You Score " + score.ToString("000000"), "OK");
+	}
+
+	void DrawStart()
+	{
+		if (GUILayout.Button("Start"))
+		{
+			Init();
+		}
+	}
+
+	void DrawGameover()
+	{
+		if (GUILayout.Button("ReStart"))
+		{
+			Init();
+		}
 	}
 
 	void DrawButton()
 	{
-		if (GUILayout.Button(""))
-			InstantiateBlock();
-
 		if (GUILayout.Button("Rotate"))
 			Rotate();
-
 		EditorGUILayout.BeginHorizontal();
 		if (GUILayout.Button("Left"))
 			MoveCheck(-1, 0);
@@ -254,7 +324,7 @@ public class GameTetorisEditor : Editor
 			MoveCheck(1, 0);
 		EditorGUILayout.EndHorizontal();
 		if (GUILayout.Button("Down"))
-			MoveCheck(0, 1);
+			Fall();
 	}
 
 	Color GetColor(int _massNum)
