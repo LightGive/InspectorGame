46ed15cad71932471be9f2cae1933e6fbb8605e3
12LJJY1\User <roi.akase@outlook.jp>
2017-06-12 02:08:05 +0900

    エラーが出る不具合修正

--- InspectorGame/Assets/GameTictactoe/GameTictactoe.cs
+++ InspectorGame/Assets/GameTictactoe/GameTictactoe.cs
@@ -33,7 +33,10 @@ public class GameTictactoeEditor : Editor
 	public override void OnInspectorGUI()
 	{
 		if (!isInit)
+		{
 			DrawSelectButton();
+			return;
+		}
 
 		EditorGUILayout.BeginVertical();
 		for (int i = 0; i < CELL_COUNT; i++)
